500f3b97f68014c9540ab2450c223e5ad0fc16b4
Muhammad Talha <muhammadtalha3810@gmail.com>
2021-06-08 13:45:16 +0500

    Remove background colors.

--- src/app/main/air/AirList.js
+++ src/app/main/air/AirList.js
@@ -59,54 +59,54 @@ class AirList extends Component {
                             filterable: false,
                             className: 'justify-center font-bold',
 
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                        rowInfo.row.ch4 < 50 ? "#00E000" : rowInfo.row.ch4 < 100 ? "#FFFF00": rowInfo.row.ch4 < 150 ? "#FF7600": rowInfo.row.ch4 < 200 ? "#FF0000": rowInfo.row.ch4 < 300 ? "#990049": rowInfo.row.ch4 < 400 ? "#7E0023": rowInfo.row.ch4 > 400 ? "#3E0023" : null
-                                    }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //             rowInfo.row.ch4 < 50 ? "#00E000" : rowInfo.row.ch4 < 100 ? "#FFFF00": rowInfo.row.ch4 < 150 ? "#FF7600": rowInfo.row.ch4 < 200 ? "#FF0000": rowInfo.row.ch4 < 300 ? "#990049": rowInfo.row.ch4 < 400 ? "#7E0023": rowInfo.row.ch4 > 400 ? "#3E0023" : null
+                            //         }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         {
                             Header: 'CO',
                             accessor: 'co',
                             filterable: false,
                             className: 'justify-center font-bold',
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                      rowInfo.row.co < 4.4 ? "#00E000" : rowInfo.row.co < 9.4 ? "#FFFF00": rowInfo.row.co < 12.4 ? "#FF7600": rowInfo.row.co < 15.4 ? "#FF0000": rowInfo.row.co < 30.4 ? "#990049": rowInfo.row.co < 40.4 ? "#7E0023": rowInfo.row.co > 40.4 ? "#3E0023" : null
-                                    }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //           rowInfo.row.co < 4.4 ? "#00E000" : rowInfo.row.co < 9.4 ? "#FFFF00": rowInfo.row.co < 12.4 ? "#FF7600": rowInfo.row.co < 15.4 ? "#FF0000": rowInfo.row.co < 30.4 ? "#990049": rowInfo.row.co < 40.4 ? "#7E0023": rowInfo.row.co > 40.4 ? "#3E0023" : null
+                            //         }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         {
                             Header: 'Dust',
                             accessor: 'dust',
                             filterable: false,
                             className: 'justify-center font-bold',
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                      rowInfo.row.dust < 12 ? "#00E000" : rowInfo.row.dust < 35.4 ? "#FFFF00": rowInfo.row.dust < 55.4 ? "#FF7600": rowInfo.row.dust < 150.4 ? "#FF0000": rowInfo.row.dust < 250.4 ? "#990049": rowInfo.row.dust < 350.4 ? "#7E0023": rowInfo.row.dust > 350.4 ? "#3E0023" : null  
-                                      }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //           rowInfo.row.dust < 12 ? "#00E000" : rowInfo.row.dust < 35.4 ? "#FFFF00": rowInfo.row.dust < 55.4 ? "#FF7600": rowInfo.row.dust < 150.4 ? "#FF0000": rowInfo.row.dust < 250.4 ? "#990049": rowInfo.row.dust < 350.4 ? "#7E0023": rowInfo.row.dust > 350.4 ? "#3E0023" : null  
+                            //           }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         
                         {
@@ -114,54 +114,54 @@ class AirList extends Component {
                             accessor: 'nh3',
                             filterable: false,
                             className: 'justify-center font-bold',
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                      rowInfo.row.nh3 < 200 ? "#00E000" : rowInfo.row.nh3 < 400 ? "#FFFF00": rowInfo.row.nh3 < 800 ? "#FF7600": rowInfo.row.nh3 < 1200 ? "#FF0000": rowInfo.row.nh3 < 1800 ? "#990049": rowInfo.row.nh3 < 2000 ? "#7E0023": rowInfo.row.nh3 > 2000 ? "#3E0023" : null  
-                                    }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //           rowInfo.row.nh3 < 200 ? "#00E000" : rowInfo.row.nh3 < 400 ? "#FFFF00": rowInfo.row.nh3 < 800 ? "#FF7600": rowInfo.row.nh3 < 1200 ? "#FF0000": rowInfo.row.nh3 < 1800 ? "#990049": rowInfo.row.nh3 < 2000 ? "#7E0023": rowInfo.row.nh3 > 2000 ? "#3E0023" : null  
+                            //         }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         {
                             Header: 'NO2',
                             accessor: 'no2',
                             filterable: false,
                             className: 'justify-center font-bold',
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                      rowInfo.row.no2 < 0.053 ? "#00E000" : rowInfo.row.no2 < 0.1 ? "#FFFF00": rowInfo.row.no2 < 0.36 ? "#FF7600": rowInfo.row.no2 < 0.65 ? "#FF0000": rowInfo.row.no2 < 1.24 ? "#990049": rowInfo.row.no2 <= 1.64 ? "#7E0023": rowInfo.row.no2 > 1.64 ? "#3E0023" : null  
-                                    }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //           rowInfo.row.no2 < 0.053 ? "#00E000" : rowInfo.row.no2 < 0.1 ? "#FFFF00": rowInfo.row.no2 < 0.36 ? "#FF7600": rowInfo.row.no2 < 0.65 ? "#FF0000": rowInfo.row.no2 < 1.24 ? "#990049": rowInfo.row.no2 <= 1.64 ? "#7E0023": rowInfo.row.no2 > 1.64 ? "#3E0023" : null  
+                            //         }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         {
                             Header: 'CO2',
                             accessor: 'co2',
                             filterable: false,
                             className: 'justify-center font-bold',
-                            getProps: (state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                  return {
-                                    style: {
-                                      background:
-                                      rowInfo.row.co2 < 1000 ? "#00E000" : rowInfo.row.co2 < 2000 ? "#FFFF00": rowInfo.row.co2 < 5000 ? "#FF7600": rowInfo.row.co2 < 10000 ? "#FF0000": rowInfo.row.co2 < 20000 ? "#990049": rowInfo.row.co2 <= 40000 ? "#7E0023": rowInfo.row.co2 > 40000 ? "#3E0023" : null  
-                                      }
-                                  };
-                                } else {
-                                  return {};
-                                }
-                              },
+                            // getProps: (state, rowInfo) => {
+                            //     if (rowInfo && rowInfo.row) {
+                            //       return {
+                            //         style: {
+                            //           background:
+                            //           rowInfo.row.co2 < 1000 ? "#00E000" : rowInfo.row.co2 < 2000 ? "#FFFF00": rowInfo.row.co2 < 5000 ? "#FF7600": rowInfo.row.co2 < 10000 ? "#FF0000": rowInfo.row.co2 < 20000 ? "#990049": rowInfo.row.co2 <= 40000 ? "#7E0023": rowInfo.row.co2 > 40000 ? "#3E0023" : null  
+                            //           }
+                            //       };
+                            //     } else {
+                            //       return {};
+                            //     }
+                            //   },
                         },
                         {
                             Header: 'Humidity',
